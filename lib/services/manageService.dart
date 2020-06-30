5832751531a9ca1e87a499f06abf0b31c49fefea
ramonyos <66652527+ramonyos@users.noreply.github.com>
2020-06-30 15:22:13 +0700

    [TASK] reflector-code 2

--- lib/services/manageService.dart
+++ lib/services/manageService.dart
@@ -1,2 +1,9 @@
-final baseUrl = 'http://119.82.252.42:10410/ONLWeb/HTT/OpnInHttSysApi/';
+//PRO
+// final baseUrl = 'http://10.168.30.7:10410/ONLWeb/HTT/OpnInHttSysApi/';
+
+//UAT
+// final baseUrl = 'http://10.168.40.6:10410/ONLWeb/HTT/OpnInHttSysApi/';
+
+//DEV
+final baseUrl = 'http://10.168.40.12:10410/ONLWeb/HTT/OpnInHttSysApi/';
 final fireBaseUrl = "https://cff-application-test.firebaseio.com/userID.json";
