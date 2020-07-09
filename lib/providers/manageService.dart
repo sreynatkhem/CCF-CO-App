7292a91d1ae4cd27ca24187c61f0abcbd217d6ce
ramonyos <66652527+ramonyos@users.noreply.github.com>
2020-07-09 11:25:58 +0700

    [FIX] fix number call loan detail

--- lib/providers/manageService.dart
+++ lib/providers/manageService.dart
@@ -1,5 +1,6 @@
 //PRO
 // final baseUrl = 'https://10.168.30.7:10413/ONLWeb/HTT/OpnInHttSysApi/';
+// final baseUrl = 'http://119.82.252.42:10410/ONLWeb/HTT/OpnInHttSysApi/';
 
 //UAT
 // final baseUrl = 'http://10.168.40.6:10413/ONLWeb/HTT/OpnInHttSysApi/';
