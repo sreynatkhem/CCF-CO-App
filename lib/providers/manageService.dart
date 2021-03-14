99135677ad8453ada4089961e66b21d876dbee27
ramonyos <66652527+ramonyos@users.noreply.github.com>
2021-03-14 14:21:09 +0700

    [TASK] create base url internal

--- lib/providers/manageService.dart
+++ lib/providers/manageService.dart
@@ -10,7 +10,8 @@ final baseUrl = 'https://10.168.30.7:10410/ONLWeb/HTT/OpnInHttSysApi/';
 // final baseUrl = 'http://10.168.40.12:10410/ONLWeb/HTT/OpnInHttSysApi/';
 // final baseUrl = 'http://119.82.252.42:10410/ONLWeb/HTT/OpnInHttSysApi/';
 
-// UAT INTERNAL
-
 // final baseURLInternal = 'http://119.82.252.42:2020/api/';
-final baseURLInternal = 'https://192.168.111.92:5001/api/';
+// final baseURLInternal = 'https://192.168.111.92:5001/api/';
+
+// CO UAT MOBILE URL
+final baseURLInternal = 'http://119.82.252.42:2031/api/';
